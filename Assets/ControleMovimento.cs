9b6a28a2ced158971c2f4bdcd4d35783805480e0
Andre Luis Ferreira <andre.luisfs01@gmail.com>
2015-04-23 16:11:07 -0300

    Commit inicial, animações, sprites e script de movimentação

---
+++ Assets/ControleMovimento.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControleMovimento : MonoBehaviour {
+	public Animator animator;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Movimentacao ();
+	}
+
+	void Movimentacao() {
+		if (Input.GetAxisRaw ("Horizontal") > 0) {
+						animator.SetBool ("andar", true);
+		}
+		if (Input.GetAxisRaw("Horizontal") < 0) {
+			animator.SetBool( "andar",false );
+		}
+	}
+}
