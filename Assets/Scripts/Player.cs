0f2704a174f72515df243fa614d00f5bed7d2086
Neils01 <iurryy@yahoo.com.br>
2015-05-11 19:30:17 -0300

    alguma coisa não commitada antes kkkkk

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,13 +6,17 @@ public class Player : MonoBehaviour {
 	public int Speed;
 	public bool pulando;
 	public GameObject Flecha;
-	private int anguloY;
+	private float anguloY;
+	private float anguloX;
+	private float forcaPulo;
 
 	// Use this for initialization
 	void Start () {
 		Speed = 10;
 		pulando = true;
 		anguloY = 500;
+		anguloX = 1000;
+		forcaPulo = 40;
 	}
 	
 	// Update is called once per frame
@@ -22,7 +26,7 @@ public class Player : MonoBehaviour {
 
 	public void jump() {
 		if (!pulando) {
-			GetComponent<Rigidbody2D>().AddForce (Vector2.up * Speed * 40);
+			GetComponent<Rigidbody2D>().AddForce (Vector2.up * Speed * forcaPulo);
 			pulando = true;
 		}
 	}
@@ -30,7 +34,7 @@ public class Player : MonoBehaviour {
 	public void atk() {
 		GameObject flecha = GameObject.Instantiate(Flecha) as GameObject;
 		flecha.GetComponent<Transform> ().position = this.transform.position;
-		flecha.GetComponent<Rigidbody2D> ().AddForce (this.transform.position + new Vector3(1000,anguloY,0));
+		flecha.GetComponent<Rigidbody2D> ().AddForce (new Vector3(anguloX,anguloY,0));
 	}
 
 	public void UpAngulo() {
@@ -39,6 +43,21 @@ public class Player : MonoBehaviour {
 
 	public void DonwAngulo() {
 		anguloY -= 100;
+	}
+
+	public void SlowTime() {
+		if (Time.timeScale == 1.0F) {
+			Time.timeScale = 0.3F;
+			anguloX = anguloX * 3.3F;
+			anguloY = anguloY * 3.3F;
+			forcaPulo = forcaPulo * 3.2F;
+		} else {
+			Time.timeScale = 1.0F;
+			anguloX = anguloX / 3.3F;
+			anguloY = anguloY / 3.3F;
+			forcaPulo = forcaPulo / 3.2F;
+		}
+		Time.fixedDeltaTime = 0.02F * Time.timeScale;
 	}
 
 	void OnCollisionEnter2D(Collision2D collision) {
